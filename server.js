7754235c1df33149a9f2a50b5ab877affae52960
Mateusz Matyskiel <346677@uwr.edu.pl>
2025-01-09 16:05:23 +0100

    feat: add login.ejs view

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.get('/', (req, res) => {
   res.send('TEST');
 });
