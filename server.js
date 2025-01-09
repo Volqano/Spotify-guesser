cad84e14f760986d232b6e5a17e069a37239416d
Mateusz Matyskiel <346677@uwr.edu.pl>
2025-01-09 15:54:27 +0100

    feat: add server.js
    Add server.js using express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('TEST');
+});
+
+app.listen(port, () => {
+  console.log('Server is listening on port ' + port);
+});
